bcbd8af421d5cb1b5e97836d0e521cd7d51de766
Waycool05 <waycool05@hotmail.com>
2025-06-19 22:13:37 -0300

    version2

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 console.log("El script se ha cargado correctamente.");
 const titulo =document.getElementById("titulo");
-titulo.textContent  ="texto modificado2";
+titulo.textContent  ="texto modificado3";
